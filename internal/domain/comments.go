126c2165f00f6a4b146a4619223f12ad1508e182
BogdanStaziyev <94914224+BogdanStaziyev@users.noreply.github.com>
2022-10-28 10:49:56 +0300

    fix: lint

--- internal/domain/comments.go
+++ internal/domain/comments.go
@@ -2,7 +2,7 @@ package domain
 
 type Comment struct {
 	ID     int64  `json:"id" example:"1"`
-	PostID int64  `json:"post_id,notnull" example:"3" validate:"required"`
+	PostID int64  `json:"post_id" example:"3" validate:"required"`
 	Name   string `json:"name" example:"Bohdan" validate:"required"`
 	Email  string `json:"email" example:"example@mail.com" validate:"required"`
 	Body   string `json:"body" example:"lorem ipsum" validate:"required"`
